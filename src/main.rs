9b16d37cb247c0f6c7b1475dc834ad637ceedea7
est31 <MTest31@outlook.com>
2018-11-02 23:58:25 +0100

    Obtain the format

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate cmp;
+extern crate ogg;
 
 extern crate hyper;
 extern crate openssl;
@@ -9,7 +10,9 @@ extern crate percent_encoding;
 
 extern crate structopt;
 
-use std::io::{self, Write};
+use ogg::PacketReader;
+
+use std::io::Cursor;
 
 use hyper::{Client, Request, Body};
 use hyper::header::USER_AGENT;
@@ -24,11 +27,12 @@ use percent_encoding::{utf8_percent_encode, DEFAULT_ENCODE_SET};
 
 use structopt::StructOpt;
 
+use ogg_metadata_extracted::BareOggFormat;
+
 // user agent to use
 const AGENT :&str = "lewton wiki tool";
 
 #[derive(StructOpt, Debug)]
-//#[structopt]
 enum Options {
 	#[structopt(name = "get")]
 	Get {
@@ -92,6 +96,58 @@ fn create_client() -> Client<HttpsConnector<HttpConnector>> {
 	client
 }
 
+fn get_ogg_format_of_stream(content :&[u8]) -> Result<BareOggFormat, String> {
+	let rdr = Cursor::new(content);
+	let mut pck_rdr = PacketReader::new(rdr);
+	let pck = try!(pck_rdr.read_packet_expected().map_err(|e| format!("{}", e)));
+	let format = try!(ogg_metadata_extracted::identify_packet_data_by_magic(&pck.data).ok_or("Unrecognized format"));
+	Ok(format.1)
+}
+
+/// Stuff taken from the ogg_metadata crate's source code
+mod ogg_metadata_extracted {
+
+	#[derive(Debug, Copy, Clone, PartialEq)]
+	pub enum BareOggFormat {
+		Vorbis,
+		Opus,
+		Theora,
+		Speex,
+		Skeleton,
+	}
+
+	pub fn identify_packet_data_by_magic(pck_data :&[u8]) -> Option<(usize, BareOggFormat)> {
+		// Magic sequences.
+		// https://www.xiph.org/vorbis/doc/Vorbis_I_spec.html#x1-620004.2.1
+		let vorbis_magic = &[0x01, 0x76, 0x6f, 0x72, 0x62, 0x69, 0x73];
+		// https://tools.ietf.org/html/rfc7845#section-5.1
+		let opus_magic = &[0x4f, 0x70, 0x75, 0x73, 0x48, 0x65, 0x61, 0x64];
+		// https://www.theora.org/doc/Theora.pdf#section.6.2
+		let theora_magic = &[0x80, 0x74, 0x68, 0x65, 0x6f, 0x72, 0x61];
+		// http://www.speex.org/docs/manual/speex-manual/node8.html
+		let speex_magic = &[0x53, 0x70, 0x65, 0x65, 0x78, 0x20, 0x20, 0x20];
+		// https://wiki.xiph.org/Ogg_Skeleton_4#Ogg_Skeleton_version_4.0_Format_Specification
+		let skeleton_magic = &[0x66, 105, 115, 104, 101, 97, 100, 0];
+
+		if pck_data.len() < 1 {
+			return None;
+		}
+
+		use self::BareOggFormat::*;
+		let ret :(usize, BareOggFormat) = match pck_data[0] {
+			0x01 if pck_data.starts_with(vorbis_magic) => (vorbis_magic.len(), Vorbis),
+			0x4f if pck_data.starts_with(opus_magic) => (opus_magic.len(), Opus),
+			0x80 if pck_data.starts_with(theora_magic) => (theora_magic.len(), Theora),
+			0x53 if pck_data.starts_with(speex_magic) => (speex_magic.len(), Speex),
+			0x66 if pck_data.starts_with(skeleton_magic) => (speex_magic.len(), Skeleton),
+
+			_ => return None,
+		};
+
+		return Some(ret);
+	}
+}
+
 fn fetch_url<T :'static + Sync + Connect>(client :&Client<T>, url :hyper::Uri) -> impl Future<Item=(), Error=()> {
 
 	let mut req = Request::builder();
@@ -107,12 +163,12 @@ fn fetch_url<T :'static + Sync + Connect>(client :&Client<T>, url :hyper::Uri) -
 			println!("Response version: {:?}", res.version());
 			println!("Headers: {:#?}", res.headers());
 
-			// The body is a stream, and for_each returns a new Future
-			// when the stream is finished, and calls the closure on
-			// each chunk of the body...
-			res.into_body().for_each(|chunk| {
-				io::stdout().write_all(format!("chunk with length {}. ", chunk.len()).as_bytes())
-					.map_err(|e| panic!("example expects stdout is open, error={}", e))
+			res.into_body().concat2().map(|body| {
+				println!("body is {} characters long", body.len());
+				//body.
+				let format = get_ogg_format_of_stream(&body);
+				println!("format is: {:?}", format);
+
 			})
 		})
 		// If all good, just tell the user...
