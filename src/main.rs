5f0101f6f7c7faf82bd05f14a49213739cf335c4
est31 <MTest31@outlook.com>
2018-11-03 00:30:16 +0100

    Implement comparison
    We remove the file format sniffing again
    because apparently all the videos that end
    in .ogg are only redirection pages now,
    and trying to get the raw media content
    results in 404 erorrs anyway, so the
    likelihood that we download a big ogg
    file that turns out to be a video
    is very low.
    
    This problem was the whole reason for
    why format sniffing was thought to be
    needed, now that it turns out to not
    be a problem at all we can
    remove sniffing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate cmp;
-extern crate ogg;
 
 extern crate hyper;
 extern crate openssl;
@@ -9,8 +8,6 @@ extern crate md5;
 extern crate percent_encoding;
 
 extern crate structopt;
-
-use ogg::PacketReader;
 
 use std::io::Cursor;
 
@@ -26,8 +23,6 @@ use tokio::runtime::current_thread::Runtime;
 use percent_encoding::{utf8_percent_encode, DEFAULT_ENCODE_SET};
 
 use structopt::StructOpt;
-
-use ogg_metadata_extracted::BareOggFormat;
 
 // user agent to use
 const AGENT :&str = "lewton wiki tool";
@@ -96,64 +91,11 @@ fn create_client() -> Client<HttpsConnector<HttpConnector>> {
 	client
 }
 
-fn get_ogg_format_of_stream(content :&[u8]) -> Result<BareOggFormat, String> {
-	let rdr = Cursor::new(content);
-	let mut pck_rdr = PacketReader::new(rdr);
-	let pck = try!(pck_rdr.read_packet_expected().map_err(|e| format!("{}", e)));
-	let format = try!(ogg_metadata_extracted::identify_packet_data_by_magic(&pck.data).ok_or("Unrecognized format"));
-	Ok(format.1)
-}
-
-/// Stuff taken from the ogg_metadata crate's source code
-mod ogg_metadata_extracted {
-
-	#[derive(Debug, Copy, Clone, PartialEq)]
-	pub enum BareOggFormat {
-		Vorbis,
-		Opus,
-		Theora,
-		Speex,
-		Skeleton,
-	}
-
-	pub fn identify_packet_data_by_magic(pck_data :&[u8]) -> Option<(usize, BareOggFormat)> {
-		// Magic sequences.
-		// https://www.xiph.org/vorbis/doc/Vorbis_I_spec.html#x1-620004.2.1
-		let vorbis_magic = &[0x01, 0x76, 0x6f, 0x72, 0x62, 0x69, 0x73];
-		// https://tools.ietf.org/html/rfc7845#section-5.1
-		let opus_magic = &[0x4f, 0x70, 0x75, 0x73, 0x48, 0x65, 0x61, 0x64];
-		// https://www.theora.org/doc/Theora.pdf#section.6.2
-		let theora_magic = &[0x80, 0x74, 0x68, 0x65, 0x6f, 0x72, 0x61];
-		// http://www.speex.org/docs/manual/speex-manual/node8.html
-		let speex_magic = &[0x53, 0x70, 0x65, 0x65, 0x78, 0x20, 0x20, 0x20];
-		// https://wiki.xiph.org/Ogg_Skeleton_4#Ogg_Skeleton_version_4.0_Format_Specification
-		let skeleton_magic = &[0x66, 105, 115, 104, 101, 97, 100, 0];
-
-		if pck_data.len() < 1 {
-			return None;
-		}
-
-		use self::BareOggFormat::*;
-		let ret :(usize, BareOggFormat) = match pck_data[0] {
-			0x01 if pck_data.starts_with(vorbis_magic) => (vorbis_magic.len(), Vorbis),
-			0x4f if pck_data.starts_with(opus_magic) => (opus_magic.len(), Opus),
-			0x80 if pck_data.starts_with(theora_magic) => (theora_magic.len(), Theora),
-			0x53 if pck_data.starts_with(speex_magic) => (speex_magic.len(), Speex),
-			0x66 if pck_data.starts_with(skeleton_magic) => (speex_magic.len(), Skeleton),
-
-			_ => return None,
-		};
-
-		return Some(ret);
-	}
-}
-
 fn fetch_url<T :'static + Sync + Connect>(client :&Client<T>, url :hyper::Uri) -> impl Future<Item=(), Error=()> {
 
 	let mut req = Request::builder();
 	req.uri(url)
-		.header(USER_AGENT, AGENT)
-		.header("Range", "bytes=0-1023");
+		.header(USER_AGENT, AGENT);
 
 	client
 		.request(req.body(Body::empty()).unwrap())
@@ -165,10 +107,10 @@ fn fetch_url<T :'static + Sync + Connect>(client :&Client<T>, url :hyper::Uri) -
 
 			res.into_body().concat2().map(|body| {
 				println!("body is {} characters long", body.len());
-				//body.
-				let format = get_ogg_format_of_stream(&body);
-				println!("format is: {:?}", format);
-
+				let cursor1 = Cursor::new(&body);
+				let cursor2 = Cursor::new(&body);
+				let res = cmp::cmp_output(cursor1, cursor2);
+				println!("Comparison result: {:?}", res);
 			})
 		})
 		// If all good, just tell the user...
